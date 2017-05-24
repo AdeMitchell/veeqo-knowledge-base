4dcea405642b654cf77259ee6d7af22949bc95e8
Jordan Hudgens <adrian@veeqo.com>
2017-05-24 12:37:16 +0000

    Integrated new and create actions for department

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,4 +2,22 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all
   end
+  
+  def new
+    @department = Department.new
+  end
+  
+  
+  def create
+    @department = Department.new(params.require(:department).permit(:title, :subtitle,  :body))
+
+    respond_to do |format|
+      if @department.save
+        format.html { redirect_to departments_path, notice: 'Department was successfully published.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
 end
