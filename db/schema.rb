cf29e08b9b34f469efc8de1f77173a9c58ee3802
Jordan Hudgens <adrian@veeqo.com>
2017-06-01 11:43:05 +0000

    Updated status fror enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601110951) do
+ActiveRecord::Schema.define(version: 20170601113357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 20170601110951) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_articles_on_slug", unique: true, using: :btree
   end
 
